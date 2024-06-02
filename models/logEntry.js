0c88e8605b4ec523aada3f5a17c66b207c419937
SAYAK MONDAL <sayakmondal2001@gmail.com>
2024-06-02 08:37:07 +0530

    Fully created /security/entry endpoint(SUCCESS)

--- models/logEntry.js
+++ models/logEntry.js
@@ -1,40 +1,38 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
 const logSchema = new mongoose.Schema({
-    roll_number:{
-        type:Number,
-        required:true,
-        unique:true
-    },
-    name:{
-        type:String,
-        required:true
-    },
-    contact:{
-        type:Number,
-        required: true,
-        unique:true
-    },
-    hostel:{
-        type:String,
-        required:true
-    },
-    room_number:{
-        type:Number,
-        required:true
-    },
-    goingTo:{
-        type:String,
-        required:true
-    },
-    outTime:{
-        type:String,
-        required:true
-    },
-    inTime:{
-        type:String,
-    }
-})
+  roll_number: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
+  hostel: {
+    type: String,
+    required: true,
+  },
+  room_number: {
+    type: Number,
+    required: true,
+  },
+  goingTo: {
+    type: String,
+    required: true,
+  },
+  outTime: {
+    type: String,
+    required: true,
+  },
+  inTime: {
+    type: String,
+  },
+});
 
-const LogEntry = mongoose.model('LogEntry', logSchema)
-module.exports=LogEntry
+const LogEntry = mongoose.model("LogEntry", logSchema);
+module.exports = LogEntry;
--- routers/studentRoute.js
+++ routers/studentRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Student = require("../models/Student");
 const getStudentByRollNumber = require("../Utils/getStudentByRollNumber");
 const updateLogBook = require("../Utils/updateLogBook");
+const LogEntry = require("../models/LogEntry");
 const router = express.Router();
 
 /*
@@ -50,12 +51,10 @@ router.get("/:rollNumber", async (req, res) => {
       res.status(200).json(data);
     } else {
       console.log(`data of Roll Number:${rollNumber} not found`);
-      res
-        .status(404)
-        .json({
-          Error: "Not Found",
-          Details: `No data of Roll Number:${rollNumber} is found`,
-        });
+      res.status(404).json({
+        Error: "Not Found",
+        Details: `No data of Roll Number:${rollNumber} is found`,
+      });
     }
   } catch (err) {
     console.log(err);
@@ -64,7 +63,6 @@ router.get("/:rollNumber", async (req, res) => {
       .json({ Error: "Internal server error", Details: err.message });
   }
 });
-
 
 router.put("/:id", async (req, res) => {
   //Updating document through id
@@ -103,38 +101,82 @@ router.delete("/:id", async (req, res) => {
 
 /*
 ::::::::::::::
-ENTRY ENDPOINT
+EXIT ENDPOINT
 ::::::::::::::
 */
-router.post('/exit', async (req, res) => {
+router.post("/exit", async (req, res) => {
   try {
     const roll = req.body.roll_number;
     const goingTo = req.body.goingTo;
     const stu = await getStudentByRollNumber(roll);
-    
+
     if (!stu) {
-      console.log('Roll Number not found');
-      return res.status(404).json({ Message: 'Roll Number not found' });
+      console.log("Roll Number not found");
+      return res.status(200).json({ Message: "Roll Number not found" });
     }
-    
+
+    const logEntry = await LogEntry.findOne({
+      roll_number: roll,
+      inTime: null,
+    });
+    if (logEntry && !logEntry.inTime) {
+      console.log("Active log found / Student already out of campus");
+      return res.status(200).json({ Message: "Active log found / Student Already out of campus" });
+    }
+
     const newLogEntry = await updateLogBook(stu, goingTo);
     res.status(200).json(newLogEntry);
-    
   } catch (error) {
-    if (error.message.includes('No data of Roll Number')) {
+    if (error.message.includes("No data of Roll Number")) {
       res.status(404).json({
-        Error: 'Not Found',
+        Error: "Not Found",
         Details: error.message,
       });
     } else {
       res.status(500).json({
-        Error: 'Internal server error',
+        Error: "Internal server error",
         Details: error.message,
       });
     }
   }
 });
 
+/*
+:::::::::::::
+ENTRY ENDPOINT
+:::::::::::::
+*/
+router.put("/entry/:rollNumber", async (req, res) => {
+  try {
+    const roll = req.params.rollNumber;
+    const logEntry = await LogEntry.findOne({
+      roll_number: roll,
+      inTime: null,
+    });
+    console.log(logEntry);
+    if (!logEntry) {
+      console.log("Log Entry not found / Student already in campus");
+      return res.status(200).json({ Message: "Log Entry not found / Student already in campus" });
+    }
+    // if (logEntry.inTime != null) {
+    //   console.log("Student already in campus");
+    //   return res.status(200).json({ Message: "Student already in campus" });
+    // }
+    const currentDate = new Date();
+    const time = currentDate.toLocaleString();
+    const logEntryToUpdate = await LogEntry.findOneAndUpdate(
+      { roll_number: roll, inTime: null },
+      { $set: { inTime: time } },
+      { new: true }
+    );
+
+    console.log("Log Entry Updated");
+    return res.status(200).json({ Message: "Log Entry updated" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ Error: "Internal server error", Details: err.message });
+  }
+});
+
 module.exports = router;
-
-
