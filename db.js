137735c427c1ee67cb9db7428045c650dad8f0ea
SAYAK MONDAL <sayakmondal2001@gmail.com>
2024-06-03 08:10:41 +0530

    MongoDB server URL shifted from local to cluster(SUCCESS)

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-const mongoURL= process.env.MONGODB_URL_LOCAL
+// const mongoURL= process.env.MONGODB_URL_LOCAL
+const mongoURL= process.env.MONGODB_URL
 
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
